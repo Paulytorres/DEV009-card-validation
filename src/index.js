7a7745ec8ba20accfa0da16355c43758b37e1ab3
Paulina Torres <axs.pau@gmail.com>
2023-06-13 22:44:04 -0400

    Version Funcional
    Se agregan las funciones de js mas html y css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,59 @@
 import validator from './validator.js';
 
-console.log(validator);
+document.getElementById('openModal').addEventListener('click', function() {
+  document.getElementById('myModal').style.display = 'block';
+});
+
+document.getElementsByClassName('close')[0].addEventListener('click', function() {
+  document.getElementById('myModal').style.display = 'none';
+});
+
+document.getElementById('validaTC').addEventListener('click', function() {
+  const creditCardNumber = document.getElementById('inputNumero').value;
+  // Aplica la máscara al número de tarjeta utilizando validator.maskify()
+  const isValidCard = validator.isValid(creditCardNumber);
+  if(isValidCard)
+  {
+    const maskedNumber =  validator.maskify(creditCardNumber);
+    const formattedNumber = formatCardNumber(maskedNumber);
+    document.getElementById('inputNumero').value = formattedNumber;
+    document.getElementById('myModal').style.display = 'none';
+    alert("subcripcion realizada con exito ");
+
+  }
+  else{
+    alert("Tarjeta invalida");
+  }
+
+});
+
+document.getElementById('inputNumero').addEventListener('input', function(event) {
+  // Obtiene el valor del campo inputNumero
+  const cardNumber = event.target.value;
+
+  // Formatea el número de tarjeta utilizando la función formatCardNumber
+  const formattedNumber = formatCardNumber(cardNumber);
+  //4137894711755904
+  const isValidCard = validator.isValid(formattedNumber);
+  document.getElementById('isvalid').textContent = isValidCard ? "valida" :  "invalida";
+  // Asigna el valor formateado de nuevo al campo inputNumero
+  event.target.value = formattedNumber;
+});
+
+function formatCardNumber(cardNumber) {
+  // Elimina todos los espacios en blanco del número de tarjeta
+  cardNumber = cardNumber.replace(/\s/g, '');
+
+  // Divide el número de tarjeta en grupos de 4 dígitos
+  const groups = cardNumber.match(/.{1,4}/g);
+
+  // Si no hay grupos válidos, devuelve el número de tarjeta original sin cambios
+  if (!groups) {
+    return cardNumber;
+  }
+
+  // Une los grupos con espacios en blanco para formatear el número de tarjeta
+  const formattedNumber = groups.join(' ');
+
+  return formattedNumber;
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,25 @@
 const validator = {
-  // ...
+  isValid: function(cardNumber) {
+    if (cardNumber.length < 10) {
+      return false;
+    }
+    const digits = cardNumber.split('').map(Number);
+      
+    for (let i = digits.length - 2; i >= 0; i -= 2) {
+      digits[i] *= 2;
+  
+      if (digits[i] >= 10) {
+        digits[i] -= 9;
+      }
+    }
+    const sum = digits.reduce((acc, curr) => acc + curr, 0);
+    return sum % 10 === 0;
+  },
+  maskify: function(creditCardNumber) {
+    const nonSpaceChars = creditCardNumber.replace(/ /g, ''); // Eliminar espacios en blanco
+    const maskedNumber = nonSpaceChars.slice(0, -4).replace(/./g, '#') + nonSpaceChars.slice(-4);
+    return maskedNumber;
+  }
 };
 
-export default validator;
+export default validator;  
